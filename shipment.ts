40d71ea032d070f6268b87c82923b782106bc0f8
Raimon Esteve <raimon@nan-tic.com>
2018-05-14 18:52:27 +0200

    Add Customer and Internal shipment interface

--- shipment.ts
+++ shipment.ts
@@ -3,7 +3,7 @@
  * DO NOT MODIFY THIS FILE, EXTEND THE INTERFACE INSTEAD
 */
 
-export interface Shipment {
+export interface InternalShipment {
   id: number,
   state: string,
   from_location: number,
@@ -16,7 +16,30 @@ export interface Shipment {
   moves?: any[]
 }
 
-export interface ShipmentLines {
+export interface InternalShipmentLines {
+  id: number,
+  product: number,
+  'product.rec_name': string,
+  max_quantity: number,
+  quantity: number,
+  served_quantity: number,
+  'product.template.cost_price': number,
+}
+
+export interface CustomerShipment {
+  id: number,
+  state: string,
+  from_location: number,
+  to_location: number,
+  company: number,
+  reference: string,
+  planned_date?: any,
+  planned_start_date?: any,
+  code?: string,
+  moves?: any[]
+}
+
+export interface CustomerShipmentLines {
   id: number,
   product: number,
   'product.rec_name': string,
